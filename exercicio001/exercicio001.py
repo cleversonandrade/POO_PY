1c9cb87d65e94b401d5073dd605839ae3f7c08e8
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-05-28 08:32:40 -0300

    aula009_exercicio

---
+++ exercicio001/exercicio001.py
@@ -0,0 +1,13 @@
+class Veiculo:
+    def __init__(self, nome, velocidade_max, quilometro_por_litro):
+        self.nome = nome
+        self.velocidade_max = velocidade_max
+        self.quilometro_por_litro = quilometro_por_litro
+    
+    def to_str(self):
+        print(f'Nome = {self.nome}')
+        print(f'velocidade_max = {self.velocidade_max}')
+        print(f'Quilometros percorridos por litro = {self.quilometro_por_litro}')
+
+modelo_carro = Veiculo('Monza', 180, 10)
+modelo_carro.to_str()
